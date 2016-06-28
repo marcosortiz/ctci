db5ab5b1e25e5886e2d3bec1e6394e7975a78425
Marcos Ortiz <marcos.ortiz@icloud.com>
2016-06-28 17:06:01 -0500

    Adding Gemfile.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
 group :test do
+    gem 'rake'
     gem 'rspec'
+    gem "codeclimate-test-reporter", require: nil
 end
--- Gemfile.rb
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-
-gemspec
-
-group :test do
-    gem 'rspec'
-    gem 'rake'
-    gem "codeclimate-test-reporter", require: nil
-end
